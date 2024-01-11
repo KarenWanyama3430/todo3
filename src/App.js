6d19e3c61744b55a16d0796a70211484af26a12a
KarenWanyama3430 <karenwanyama3430@gmail.com>
2024-01-11 23:41:26 +0300

    changes on the todo files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import Signup from './Components/LoginSignup/Signup';
+import Login from './Components/LoginSignup/Login';
+import Header from './Components/Header/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
+import TodoWrapper from './Components/Todo/TodoWrapper';
+
+
+const App = () => {
+
+
   return (
-    <div>
-      
-    </div>
-  );
+    <Router>
+      <Header/>
+      <Routes>
+        
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/todowrapper" element={<TodoWrapper />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
